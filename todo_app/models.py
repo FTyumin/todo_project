e2099de3cd12d95565e36801941baf13e522ac3a
FTyumin <feodor.tjumin28@gmail.com>
2023-02-02 21:22:25 +0200

    Smth

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    due_date = models.DateField()
+
+    def _str_(self):
+        return self.title
