ad1a1b663e0fa91e02cbe9d0161239a97ae4307c
FTyumin <feodor.tjumin28@gmail.com>
2023-02-17 17:38:15 +0200

    A few edits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import React, { Component } from "react";
 import {Navigation} from 'react-minimal-side-navigation';
 import 'react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css';
+import {Button, Card, Form} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const todoItems = [
   {id:1,
@@ -26,52 +28,54 @@ const todoItems = [
 
 function App() {
   return (
-    <div className='sidebar'>
+    <div>
+      <div className='sidebar'>
 
-      <Navigation
-              style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}
-              // you can use your own router's api to get pathname
-              activeItemId="/management/members"
-              onSelect={({itemId}) => {
-                // maybe push to the route
-              }}
-              items={[
-                {
-                  title: 'Todo app',
-                  itemId: '/dashboard',
-                  // you can use your own custom Icon component as well
-                  // icon is optional
-                  elemBefore: () => <label name="inbox" />,
-                },
-                {
-                  title: 'ToDo',
-                  itemId: '/todo',
-                  elemBefore: () => <label name="users" />,
-                  subNav: [
-                    {
-                      title: 'Tasks',
-                      itemId: '/management/projects',
-                    },
-                    {
-                      title: 'Members',
-                      itemId: '/management/members',
-                    },
-                  ],
-                },
-                {
-                  title: 'Another Item',
-                  itemId: '/another',
-                  subNav: [
-                    {
-                      title: 'example',
-                      itemId: '/management/example',
-                    },
-                  ],
-                },
-              ]}
-            />
+        <Navigation
+                style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}
+                // you can use your own router's api to get pathname
+                activeItemId="/management/members"
+                onSelect={({itemId}) => {
+                  // maybe push to the route
+                }}
+                items={[
+                  {
+                    title: 'Todo app',
+                    itemId: '/dashboard',
+                    // you can use your own custom Icon component as well
+                    // icon is optional
+                    elemBefore: () => <label name="inbox" />,
+                  },
+                  {
+                    title: 'ToDo',
+                    itemId: '/todo',
+                    elemBefore: () => <label name="users" />,
+                    subNav: [
+                      {
+                        title: 'Tasks',
+                        itemId: '/management/projects',
+                      },
+                      {
+                        title: 'Members',
+                        itemId: '/management/members',
+                      },
+                    ],
+                  },
+                  {
+                    title: 'Chat',
+                    itemId: '/another',
+                    subNav: [
+                      {
+                        title: 'example',
+                        itemId: '/management/example',
+                      },
+                    ],
+                  },
+                ]}
+              />
 
-          </div>
+            </div>
+    </div>
 
     
       
