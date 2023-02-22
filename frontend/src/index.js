64910f3f791cf17ecdf04fe4e01218e32b1e257c
FTyumin <feodor.tjumin28@gmail.com>
2023-02-22 20:57:15 +0200

    Trying to fix sidebar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,8 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
 import Layout from './pages/layout'
 import reportWebVitals from './reportWebVitals'
-import Login from './pages/login';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,7 +19,4 @@ root.render(
 
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
